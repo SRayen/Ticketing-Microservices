c01195cc12db21e36cf5cbee22231ec12fbd6c9f
Selmen Rayen <selmanerayene95@gmail.com>
2023-08-19 07:54:03 +0100

    add cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -8,10 +10,16 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
-import mongoose from "mongoose";
 
 const app = express();
+app.set("trust proxy", true); //For Express to trust  the proxy of ingress-nginx
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false, //disable encryption: (To be understood between diff languages!) / (JWT is already encrypted)
+    secure: true, //only used with https
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
