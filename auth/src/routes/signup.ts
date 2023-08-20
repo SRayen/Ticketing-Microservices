2b52d8a205f4325c5ec992d4dd823ae342216933
Selmen Rayen <selmanerayene95@gmail.com>
2023-08-20 03:10:49 +0100

    accessing env var

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -41,7 +41,7 @@ router.post(
         id: user.id,
         email: user.email,
       },
-      "asdf"
+      process.env.JWT_KEY! //'!' : to tell TS: 'we have checked this thing (in index.ts) : (env is defined)
     );
 
     //Store it on session object : (cookieSession library is going to take this object, serialize it & send it back to the user's browser)
