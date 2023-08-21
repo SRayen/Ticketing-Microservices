e05dce0e411571b442c0d52077bf73913c8b1071
Selmen Rayen <selmanerayene95@gmail.com>
2023-08-21 01:23:36 +0100

    signin flow

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 import { RequestValidationError } from "../errors/request-validation-error";
-// import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-errors";
 
@@ -29,7 +28,7 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError("Email in use1");
+      throw new BadRequestError("Email in use");
     }
 
     const user = User.build({ email, password });
