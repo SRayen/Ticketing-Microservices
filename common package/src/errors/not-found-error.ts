c32c0e236c21d90c414a4b4b85b11f4e579a5392
Selmen Rayen <selmanerayene95@gmail.com>
2023-09-01 02:24:57 +0100

    fix

--- common package/src/errors/not-found-error.ts
+++ common package/src/errors/not-found-error.ts
@@ -1,7 +1,7 @@
 import { CustomError } from "./custom-error";
 
 export class NotFoundError extends CustomError {
-  statusCode = 400;
+  statusCode = 404;
 
   constructor() {
     super("Route not found");
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,7 +1,11 @@
 import request from "supertest"; //supertest : Allow us to fake a req to Express App
 import { app } from "../../app";
 
-it("has a route handler listening to /api/tickets for post requests", async () => {});
+it("has a route handler listening to /api/tickets for post requests", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+  console.log("==>", response.status);
+  expect(response.status).not.toEqual(404);
+});
 
 it("can only be accessed if the user is signed in", async () => {});
 
