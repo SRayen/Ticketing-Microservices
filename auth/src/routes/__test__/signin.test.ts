f4c20bfe85b06c394832f750561c9a8f9296f2b3
Selmen Rayen <selmanerayene95@gmail.com>
2023-11-06 14:41:27 +0100

    verify test auth process

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -18,7 +18,7 @@ it("fails when an incorrect password is supplied", async () => {
       email: "test@test.com",
       password: "password",
     })
-    .expect(201);
+    .expect(400);
 
   await request(app)
     .post("/api/users/signin")
