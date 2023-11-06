0fb76c1777c1bfab6b36cf5d7764256f519bdac9
Selmen Rayen <selmanerayene95@gmail.com>
2023-11-06 23:59:19 +0100

    update start auth message

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
-  console.log("Starting up .....");
+  console.log("Starting up ...");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
