f2f5969fc98b2813116cd0d1a18f15abfcf15efd
Selmen Rayen <selmanerayene95@gmail.com>
2023-11-07 12:16:07 +0100

    add comment about bull.js

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -14,3 +14,11 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     msg.ack();
   }
 }
+
+
+/* RQ:
+The expirationQueue.add() method is used to add a job to the expiration queue in Bull.js
+In our case, we are passing an object with two properties:
+1. orderId: The id of the created order
+2. delay: The time until the job should be processed (in milliseconds)
+*/
